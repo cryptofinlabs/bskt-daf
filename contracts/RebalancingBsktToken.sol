78a9f7a36dc6de2b1ecd827997a07e434fa09f00
Daniel Que <dmdque@gmail.com>
2018-07-06 16:18:13 -0700

    Copy create and redeem from Bskt v1

--- contracts/RebalancingBsktToken.sol
+++ contracts/RebalancingBsktToken.sol
@@ -1,17 +1,23 @@
 pragma solidity 0.4.24;
 
 
+import "cryptofin-solidity/contracts/array-utils/UIntArrayUtils.sol";
 import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/DetailedERC20.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/StandardToken.sol"; 
-import "cryptofin-solidity/contracts/array-utils/UIntArrayUtils.sol";
+import "zeppelin-solidity/contracts/lifecycle/Pausable.sol";
 
 import "./IBsktToken.sol";
 import "./Math.sol";
 
 
-contract RebalancingBsktToken is IBsktToken, StandardToken, DetailedERC20 {
+contract RebalancingBsktToken is
+  DetailedERC20
+  IBsktToken,
+  Pausable,
+  StandardToken,
+{
 
   using SafeMath for uint256;
   using UIntArrayUtils for uint256[];
@@ -32,7 +38,53 @@ contract RebalancingBsktToken is IBsktToken, StandardToken, DetailedERC20 {
     quantities = _quantities;
   }
 
-  function issue() public {
+  function issue(uint256 amount)
+    external
+    whenNotPaused()
+    requireNonZero(amount)
+    requireMultiple(amount)
+  {
+    // Check overflow
+    require((totalSupply_ + amount) > totalSupply_);
+
+    for (uint256 i = 0; i < tokens.length; i++) {
+      TokenInfo memory token = tokens[i];
+      ERC20 erc20 = ERC20(token.addr);
+      uint256 amount = amount.div(creationUnit).mul(token.quantity);
+      require(erc20.transferFrom(msg.sender, address(this), amount));
+    }
+
+    mint(msg.sender, amount);
+    emit Create(msg.sender, amount);
+  }
+
+  function redeem(uint256 amount, address[] tokensToSkip)
+    external
+    requireNonZero(amount)
+    requireMultiple(amount)
+  {
+    require(amount <= totalSupply_);
+    require(amount <= balances[msg.sender]);
+    require(tokensToSkip.length <= tokens.length);
+    // Total supply check not required since a user would have to have
+    // balance greater than the total supply
+
+    // Burn before to prevent re-entrancy
+    burn(msg.sender, amount);
+
+    for (uint256 i = 0; i < tokens.length; i++) {
+      TokenInfo memory token = tokens[i];
+      ERC20 erc20 = ERC20(token.addr);
+      uint256 index;
+      bool ok;
+      (index, ok) = tokensToSkip.index(token.addr);
+      if (ok) {
+        continue;
+      }
+      uint256 amount = amount.div(creationUnit).mul(token.quantity);
+      require(erc20.transfer(msg.sender, amount));
+    }
+    emit Redeem(msg.sender, amount, tokensToSkip);
   }
 
   function redeem() public {
@@ -52,6 +104,7 @@ contract RebalancingBsktToken is IBsktToken, StandardToken, DetailedERC20 {
 
   }
 
+  // `map` doesn't work if these are in a library
   function logFloor(uint256 n) public pure returns(uint256) {
     uint256 i = 0;
     while(true) {
@@ -75,6 +128,28 @@ contract RebalancingBsktToken is IBsktToken, StandardToken, DetailedERC20 {
   function creationSize() view public returns(uint256) {
     uint256 optimal = quantities.map(logFloor).reduce(min);
     return Math.max(uint256(decimals).sub(optimal), 1);
+  }
+
+  // @dev Mints new tokens
+  // @param to Address to mint to
+  // @param amount Amount to mint
+  // @return isOk Whether the operation was successful
+  function mint(address to, uint256 amount) internal returns (bool) {
+      totalSupply_ = totalSupply_.add(amount);
+      balances[to] = balances[to].add(amount);
+      emit Transfer(address(0), to, amount);
+      return true;
+  }
+
+  // @dev Burns tokens
+  // @param from Address to burn from
+  // @param amount Amount to burn
+  // @return isOk Whether the operation was successful
+  function burn(address from, uint256 amount) internal returns (bool) {
+      totalSupply_ = totalSupply_.sub(amount);
+      balances[from] = balances[from].sub(amount);
+      emit Transfer(from, address(0), amount);
+      return true;
   }
 
 }
