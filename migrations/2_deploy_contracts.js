594c41b6c51a494b51257e176942bf8f3a75ef40
Daniel Que <dmdque@gmail.com>
2018-08-17 15:23:32 -0700

    Write deployed addresses to build file

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,29 +1,52 @@
+const jsonfile = require('jsonfile');
+
 const BsktRegistry = artifacts.require('BsktRegistry');
 const ERC20Token = artifacts.require('ERC20Token');
 const Math = artifacts.require('Math');
 const RebalancingBsktToken = artifacts.require('RebalancingBsktToken');
 
 
-module.exports = function(deployer, network, accounts) {
+const DEPLOYED_ADDRESSES_PATH = './build/deployed-addresses.json';
+
+module.exports = (deployer, network, accounts) => {
   if (network == 'development') {
-    let bsktRegistry, token;
+    let bsktRegistry, feeToken, rebalancingBsktToken;
     let feeAmount = 10**17;
 
-
-    deployer.then(function() {
+    deployer.then(() => {
       return deployer.deploy(Math);
-    }).then(function() {
+    }).then(() => {
       return deployer.link(Math, [RebalancingBsktToken]);
-    }).then(function() {
+    }).then(() => {
       return ERC20Token.new();
-    }).then(function(_token) {
-      token = _token;
-      return BsktRegistry.new(accounts[1], token.address, feeAmount);
-    });
-    //.then(function(_bsktRegistry) {
-      //bsktRegistry = _bsktRegistry;
-      //console.log('token address', token.address);
-      //return RebalancingBsktToken.new([token.address], [100], bsktRegistry, 'RebalancingBsktToken', 'RBT');
-    //});
+    }).then(_feeToken => {
+      feeToken = _feeToken;
+      return BsktRegistry.new(accounts[1], feeToken.address, feeAmount);
+    }).then(_bsktRegistry => {
+      bsktRegistry = _bsktRegistry;
+      return RebalancingBsktToken.new(
+        [feeToken.address],
+        [100],
+        10**18,
+        bsktRegistry.address,
+        7 * 24 * 60 * 60,
+        0,
+        24 * 60 * 60,
+        3 * 24 * 60 * 60,
+        12 * 60 * 60,
+        24 * 60 * 60,
+        'RebalancingBsktToken',
+        'RBSKT'
+      );
+    }).then(_rebalancingBsktToken => {
+      rebalancingBsktToken = _rebalancingBsktToken;
+      let deployedAddresses = {
+        'bsktRegistry': bsktRegistry.address,
+        'feeToken': feeToken.address,
+        'rebalancingBsktToken': rebalancingBsktToken.address
+      };
+      console.log('deployed addresses:', deployedAddresses);
+      jsonfile.writeFileSync(DEPLOYED_ADDRESSES_PATH, deployedAddresses, {spaces: 2});
+    })
   }
 };
