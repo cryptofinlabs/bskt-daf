58f99611c06c7a4634bc333fe05467d447d82cb5
Daniel Que <dmdque@gmail.com>
2018-09-05 15:08:09 -0700

    Move code into library

--- contracts/impl/BidImpl.sol
+++ contracts/impl/BidImpl.sol
@@ -35,4 +35,47 @@ library BidImpl {
     return false;
   }
 
+  // Compares two bids, returning true if the first bid is better, and breaking ties with the second bid
+  // How it works:
+  // * Maps each bid into an array of percentages representing the percentage of each token filled
+  // * Requires that these percentages are sorted in increasing order, and that all positives are first
+  //   (getRebalanceDeltas takes care of separating +/-, and the bidder must construct the call such that this requirement is satisfied)
+  // * Compares the two percentage arrays by comparing the first element, and iterating
+  function compareBids(
+    address[] memory tokensA,
+    int256[] memory quantitiesA,
+    address[] memory tokensB,
+    int256[] memory quantitiesB,
+    address[] memory deltaTokens,
+    int256[] memory deltaQuantities
+  )
+  public
+  view
+  returns (bool)
+  {
+    require(tokensA.length == quantitiesA.length);
+    require(tokensA.length == tokensB.length);
+    require(tokensA.length == quantitiesB.length);
+
+    Rational.Rational256[] memory fillProportionA = new Rational.Rational256[](deltaTokens.length);
+    Rational.Rational256[] memory fillProportionB = new Rational.Rational256[](deltaTokens.length);
+    for (uint256 i = 0; i < deltaTokens.length; i++) {
+      if (deltaQuantities[i] > 0) {
+        require(quantitiesA[i] >= 0);
+        require(quantitiesB[i] >= 0);
+        fillProportionA[i] = Rational.Rational256({ n: uint256(quantitiesA[i]), d: uint256(deltaQuantities[i]) });
+        fillProportionB[i] = Rational.Rational256({ n: uint256(quantitiesB[i]), d: uint256(deltaQuantities[i]) });
+      } else {
+        // If tokens are being sold (negative delta), entry is 100
+        // It's assumed bidders act in their own economic interest, so they
+        // wouldn't leave any tokens on the table
+        // Also anything with deltaQuantity 0 is automatically 100%
+        fillProportionA[i] = Rational.Rational256({ n: 100, d: 1 });
+        fillProportionB[i] = Rational.Rational256({ n: 100, d: 1 });
+        continue;
+      }
+    }
+    return compareSortedRational256s(fillProportionA, fillProportionB);
+  }
+
 }
